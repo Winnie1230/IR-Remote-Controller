0868eca20988a9b7c8707169571ca1e467451f91
Winnie1230 <winnie1998228@gmail.com>
2019-10-18 03:29:32 +0800

    modified index.js

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,3 +1,20 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('nckuwinnieliu.ddns.net')
+
+client.on('connect', function (){
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
+
 $("#send_button").click(()=>{
     var send_topic = document.getElementById("input_topic").value;
     var send_message = document.getElementById("input_message").value;
