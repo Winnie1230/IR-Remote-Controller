cf20e5d06a1ed83573e585edb635f73fece92d48
Winne1230 <winnie1998228@gmail.com>
2019-11-26 18:26:49 +0800

    add mqtt test code(publisher and subscriber)

---
+++ server/testMQTT/subscriber.py
@@ -0,0 +1,19 @@
+# Subscriber.py
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    #client.subscribe("esp8266")
+    client.subscribe("co2")
+    client.subscribe("pm")
+    client.subscribe("current")
+    client.subscribe("temp")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("nckuwinnieliu.ddns.net", 1883, 60)
+client.loop_forever()
